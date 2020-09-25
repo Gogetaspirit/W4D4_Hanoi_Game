3b493a2249f8ea08c02f6df8c8633e8d61b7d6f4
ChrisLee <christopher.dan.lee96@gmail.com>
2020-09-24 20:46:44 -0400

    first draft

---
+++ first_tdd.rb
@@ -0,0 +1,51 @@
+class Array
+    def my_uniq
+        hash = Hash.new(0)
+        self.each do |key|
+            hash[key] += 1
+        end
+        hash.keys
+    end
+
+    def two_sum
+        sum = []
+        
+        length.times do |i| 
+            target = -self[i]
+            idx = self.index(target) 
+            if idx && self[idx] != 0 && idx > i  
+                sum << [i, idx]
+            end 
+        end 
+        sum
+    end
+
+end
+
+def my_transpose(grid)
+    row = grid.count
+    new_grid = Array.new(row) { Array.new(row)}
+
+    row.times do |i|
+        row.times do |j|
+            new_grid[j][i] = grid[i][j]
+        end
+    end
+    new_grid
+end
+
+def stock_picker(prices)
+    max = 0 
+    arr = []
+    prices.each_with_index do |buy, i|
+        prices.each_with_index do |sell, j|
+            if j > i && (sell - buy > max)
+                arr = [i, j]
+                max = sell - buy 
+            end
+        end
+    end
+    arr
+end
+
+# [9, 7, 1, 6 ,3]
---
+++ hanoi.rb
@@ -0,0 +1,37 @@
+class Hanoi
+    attr_reader :arr
+    def initialize
+        @arr = [[3,2 ,1],[],[]]#[[3,2,1],[],[]] => [[1,2][][3]] => [[2],[],[1,3]]=> 
+    end
+
+    def play
+        until won?
+            puts "Enter an start index: "
+            start_idx = gets.chomp.to_i
+            puts "Enter an ending index: "
+            end_idx = gets.chomp.to_i
+
+            # move_valid?(start_idx, end_idx)
+            # move(start_idx, end_idx)
+
+        end
+    end
+
+    def move(start_idx, end_idx)
+        # [3,2,1]=> if arr[start][i] > [start][i+1] 
+        # i = 0
+        raise "You can't move from a pile that is empty!" if arr[start_idx].empty?
+        raise "Can't place bigger disk on smaller disk!" if !arr[end_idx].empty? && arr[end_idx].first < arr[start_idx].first
+        disc = arr[start_idx].shift
+        arr[end_idx].unshift(disc)
+
+        arr
+    end
+
+    def won?
+        return @arr.one? do |subarr|
+             subarr == subarr.sort
+        end  
+        
+    end
+end
